678ed254b1ca58dd193730eefc7fe69b552c11d9
Andrey Pchelintsev <doctor.madbee@gmail.com>
2024-08-15 20:18:22 +0700

    added class ContactsOrderTypeUi changed class SortTypeUI(extended from ContactsOrderTypeUi)

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+
+public abstract class ContactsOrderTypeUi {
+    protected String type;
+    protected boolean isSelected;
+
+    public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
+        this.type = type;
+        this.isSelected = isSelected;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+}
